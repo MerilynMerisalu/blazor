fb4d3815a3ae9dedae7ceebeb6d7f45782ea6fbe
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-06-03 00:42:10 +0300

    initial commit

--- BlazorMovieAppSolution/BlazorMovieApp/Models/Movie.cs
+++ BlazorMovieAppSolution/BlazorMovieApp/Models/Movie.cs
@@ -6,11 +6,14 @@ namespace BlazorMovieApp.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
         [DataType(DataType.Date)]
+
         public DateTime? ReleaseDate { get; set; }
         public string? Genre { get; set; }
         [Precision(18, 2)]
+        [Range(0, 100)]
         public decimal Price { get; set; }
     }
 }
