bd61cef31229b85a30ba63e9467fbd01f47d5bc7
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-06-02 23:54:43 +0300

    initial commit

---
+++ BlazorMovieAppSolution/BlazorMovieApp/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMovieApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+    }
+}
