c4c677de6eb061e0d586451e8fe2053c0c1d4d18
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-06-03 21:42:18 +0300

    initial commit

---
+++ ContactBookSolution/App.DAL.EF/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using App.Domain.EF;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.DAL.EF
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; } = default!;
+        public DbSet<Contact> Contacts { get; set; } = default!;
+        public DbSet<ContactType> ContactTypes { get; set; } = default!;
+
+    }
+}
---
+++ ContactBookSolution/App.Domain.EF/ContactType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Domain.EF
+{
+    public class ContactType
+    {
+        public int ContactTypeId { get; set; }
+        [Required]
+        [MaxLength(64)]
+        [StringLength(64)]
+        public string ContactTypeName { get; set; } = default!;
+
+        public ICollection<Contact>? Contacts { get; set; }
+    }
+}
